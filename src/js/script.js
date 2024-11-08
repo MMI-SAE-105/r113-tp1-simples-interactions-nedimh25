1466cd77869eddc60ce11f5e71e773fbf86901ce
nedim <nedim14.halepovic@gmail.com>
2024-11-08 10:54:01 +0100

    code ex2

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,7 @@
 const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge");
 aMettreEnRouge.style.color = "red";
+
+const enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click");
+enRougeSuiteAClick.addEventListener("click", (evt) => {
+    enRougeSuiteAClick.style.color = "red";
+});
