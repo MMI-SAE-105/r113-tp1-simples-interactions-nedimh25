a62a5e14bc2b40190a73bed2a5cc9ef4e296fc70
nedim <nedim14.halepovic@gmail.com>
2024-11-08 10:54:23 +0100

    code exo3

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,10 @@ const enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click");
 enRougeSuiteAClick.addEventListener("click", (evt) => {
     enRougeSuiteAClick.style.color = "red";
 });
+
+const collH2 = document.querySelectorAll("h2");
+collH2.forEach((elm) => {
+    elm.addEventListener("click", (evt) => {
+        evt.target.style.color = "red";
+    });
+});
